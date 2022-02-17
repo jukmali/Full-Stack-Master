db7df55dbc0478e23ce9eb6eddce3bd680b93c6d
jukmali <54353202+jukmali@users.noreply.github.com>
2022-02-17 19:14:54 +0200

    ok

--- Lesson3_ex/ws_2/write_file.js
+++ Lesson3_ex/ws_2/write_file.js
@@ -1,6 +1,8 @@
 var fs = require("fs");
 
 console.log("Program started...");
+
+// Combine two files to one file
 var comb ="";
 fs.readFile('example.txt', function(err,data){
     if(err) console.log(err);
@@ -13,13 +15,34 @@ fs.readFile('example.txt', function(err,data){
         comb = data.toString() +"\n" + data2.toString();
         fs.writeFile('combiningfiles.txt',comb, function(err){
             if(err) throw err;
-            console.log('Files are written!')
-        })
+            console.log('Files are written!');
+            //fs.close();
+
+            console.log("Append text");
+            // Append text beginning of file and end of file
+            fs.readFile('./combiningfiles.txt', function(err,data3){
+                if(err) throw err;
+                console.log('Third file is read');
+                buf = "I added new text \n" + data3;
+                fs.writeFile('./combiningfiles.txt', buf, function(err){
+                    if(err) throw err;
+                    console.log('Top text included');
+                });
+
+                buf = '\nThis is the text in the end'
+
+                fs.appendFile('./combiningfiles.txt', buf, function(err){
+                    if(err) throw err;
+                    console.log("End text included");
+                });
+
+            });
+            
+
+        });
+        
     });
 });
-
-//comb = data.toString() + data1.toString();
-//console.log(comb);
 
 
 console.log("Program ended");
